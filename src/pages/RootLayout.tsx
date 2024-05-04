cbc0cf069029d93ec96bf65cbee2939ec2b632d7
Danilo Santos <dscofield@outlook.com.br>
2024-05-03 22:00:35 -0300

    fix: implement StyleSheetManager to filter unknown props and resolve hasError DOM error

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom'
+import { StyleSheetManager } from 'styled-components'
 
 import Header from '../components/header/header.component'
 
+const shouldForwardProp = (prop: string) => prop !== 'hasError'
+
 const RootLayout = () => {
   return (
-    <>
+    <StyleSheetManager shouldForwardProp={shouldForwardProp}>
       <Header />
       <Outlet />
-    </>
+    </StyleSheetManager>
   )
 }
 
