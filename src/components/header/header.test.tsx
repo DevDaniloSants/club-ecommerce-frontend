79529574bb846858748d38e8ed566cd0983f6a83
Danilo Santos <dscofield@outlook.com.br>
2024-06-01 19:41:45 -0300

    test: ensure cart products count are correctly shown on header

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,5 +1,6 @@
 import Header from './header.component'
 import { renderWithRedux } from '../../helpers/test-helpers'
+import CartProducts from '../../types/cartProducts.types'
 
 describe('Header', () => {
   it('should show sign out button if user is autheticated', () => {
@@ -19,5 +20,31 @@ describe('Header', () => {
 
     getByText(/login/i)
     getByText(/criar conta/i)
+  })
+
+  it('should show correct cart products count', () => {
+    const products: CartProducts[] = [
+      {
+        id: '1',
+        imageUrl: 'image.png',
+        name: 'boné',
+        price: 100,
+        quantity: 20,
+      },
+      {
+        id: '2',
+        imageUrl: 'image.png',
+        name: 'jaqueta',
+        price: 100,
+        quantity: 2,
+      },
+    ]
+
+    const { getByText } = renderWithRedux(<Header />, {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      preloadedState: { cartReducer: { products } } as any,
+    })
+
+    getByText('22')
   })
 })
